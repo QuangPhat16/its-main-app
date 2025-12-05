b6bb82e312d2b5a67f9bdfebb177dddf8067e8df
QuangPhat16 <phat.quang163204@hcmut.edu.vn>
2025-12-05 10:12:37 +0700

    integrate lessonController and quizController

--- src/course_management/modules/course.module.ts
+++ src/course_management/modules/course.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseService } from '../services/course.service';
 import { LessonService } from '../services/lesson.service';
 import { QuizService } from '../services/quiz.service';
-import { MediaService } from '../services/media.service';
 import { MediaModule } from './media.module';
 import { CourseController } from '../controllers/course.controller';
+import { LessonController } from '../controllers/lesson.controller';
+import { QuizController } from '../controllers/quiz.controller';
 import { Course } from '../entities/course.entity';
 import { Lesson, LessonContent } from '../entities/lesson.entity';
 import { Quiz, Question, Answer } from '../entities/quiz.entity';
@@ -17,7 +18,7 @@ import { AuthModule } from '../../auth/auth.module';
     AuthModule, MediaModule
   ],
   providers: [CourseService, LessonService, QuizService],
-  controllers: [CourseController],
+  controllers: [CourseController, LessonController, QuizController],
   exports: [CourseService, LessonService, QuizService], // If needed in other modules
 })
 export class CourseModule {}
